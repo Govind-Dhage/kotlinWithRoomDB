fed43b92e21c3c97efbaf6e217a583d7178eab0d
Govind Dhage <govinddhage97@gmail.com>
2024-02-05 12:00:57 +0530

    first comment

---
+++ app/src/main/java/com/govind/dhage/kotlinwithroomdb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.govind.dhage.kotlinwithroomdb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
